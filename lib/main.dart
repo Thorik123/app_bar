278526ca946b9e8d0aa01db9c62b4b4daec88f1b
Thorik123 <tronikthorik07@gmail.com>
2021-12-18 03:23:50 +0700

    commit pertama

---
+++ lib/main.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Scaffold'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Scaffold'),
+      ),
+      body: Center(
+        child: Text(
+          'Home Page',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 35.0,
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+      ),
+      //////////////////////////////////////////////////////////////
+      floatingActionButton: FloatingActionButton(
+        elevation: 10.0,
+        child: Icon(Icons.add),
+        onPressed: () {
+          print('I am Floating Button');
+        },
+      ),
+      //////////////////////////////////////////////////////////////
+      persistentFooterButtons: <Widget>[
+        ElevatedButton(
+          onPressed: () {},
+          child: Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          child: Icon(
+            Icons.clear,
+            color: Colors.white,
+          ),
+        ),
+      ],
+      //////////////////////////////////////////////////////////////
+      drawer: Drawer(
+        elevation: 16.0,
+        child: Column(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text("Thorik Tronik"),
+              accountEmail: Text("thoriktronik@mail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Text("TT"),
+              ),
+              otherAccountsPictures: <Widget>[
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Text("IN"),
+                )
+              ],
+            ),
+            ListTile(
+              title: new Text("All Inboxes"),
+              leading: new Icon(Icons.mail),
+            ),
+            ListTile(
+              title: new Text("Primary"),
+              leading: new Icon(Icons.inbox),
+            ),
+            ListTile(
+              title: new Text("Social"),
+              leading: new Icon(Icons.people),
+            ),
+            ListTile(
+                title: new Text("Promotions"),
+                leading: new Icon(Icons.local_offer)),
+          ],
+        ),
+      ),
+      ////////////////////////////////////////////////////////////////
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        fixedColor: Colors.teal,
+        items: [
+          BottomNavigationBarItem(
+            title: Text("Home"),
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            title: Text("Search"),
+            icon: Icon(Icons.search),
+          ),
+          BottomNavigationBarItem(
+            title: Text("Add"),
+            icon: Icon(Icons.add_box),
+          ),
+        ],
+        onTap: (int index) {
+          setState(() {});
+        },
+      ),
+    );
+  }
+}
